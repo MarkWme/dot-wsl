3fa6082dfcaff55bf4e403ef931f092f43d53998
Mark Whitby <mawhitby@microsoft.com>
2020-06-22 23:34:32 +0100

    Added pure prompt

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ apt install zsh zsh-syntax-highlighting zsh-autosuggestions
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Pure prompt
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+mv .zshrc $HOME
